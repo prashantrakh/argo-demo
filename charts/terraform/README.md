5db655c363ff54f9bf991007907fc1f63523ec9e
stackgen-automation <automation@stackgen.com>
2024-10-09 07:37:21 +0000

    Automated commit

---
+++ charts/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with appCD.
+You can modify your appStack -> [here](http://cloud.stackgen.com/appstacks/90f11ca0-b1ae-44ad-b94c-05d7cca743f4)
---
+++ charts/terraform/outputs.tf
@@ -0,0 +1,11 @@
+
+output "aws_iam_role_argo_demo_role_arn" {
+  value = module.appcd_8072cc9a-d640-5555-b90b-a71965928b08.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_argo_demo_role_name" {
+  value = module.appcd_8072cc9a-d640-5555-b90b-a71965928b08.name
+  sensitive = false
+}
